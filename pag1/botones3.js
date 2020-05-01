4e21249b45b1e7a42eb81b13d1092fe1787c2635
Jordy-Pedro-Valencia-Jara <jquispejar@unsa.edu.pe>
2020-05-01 18:03:21 -0500

    tarea01

---
+++ pag1/botones3.js
@@ -0,0 +1,6 @@
+function crecer(){
+    document.getElementById('palabra').style.fontSize="40px";
+}
+function disminuir(){
+    document.getElementById('palabra').style.fontSize="10px";
+}
---
+++ pag1/botones4.js
@@ -0,0 +1,8 @@
+function crecer(){
+    document.getElementById('palabra').style.fontSize="40px";
+    document.getElementById('palabra').style.color="red";
+}
+function disminuir(){
+    document.getElementById('palabra').style.fontSize="10px";
+    document.getElementById('palabra').style.color="blue";
+}
